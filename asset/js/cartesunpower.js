3af54377813b9c6b82b0733473a65c7890d4b7fd
Moulaïli Saïdi <m2xyoussouf@gmail.com>
2021-10-25 17:27:22 +0200

    v

--- asset/js/cartesunpower.js
+++ asset/js/cartesunpower.js
@@ -1,4 +1,6 @@
-var mymap = L.map('mapid').setView([45.166672 ,5.71667], 12);
+var mymap = L.map('mapid',{
+    scrollWheelZoom: false
+}).setView([45.166672 ,5.71667], 12);
                 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
