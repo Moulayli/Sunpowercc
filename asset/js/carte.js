79a97e8e0c70be6521a0ade930233492e16eef1a
Moulaïli Saïdi <m2xyoussouf@gmail.com>
2021-10-25 13:48:29 +0200

    v2

--- asset/js/carte.js
+++ asset/js/carte.js
@@ -2,7 +2,7 @@ function place() {
     let place = Math.floor(Math.random() * 7)
     return place;
 }
-var mymap = L.map('mapid').setView([43.610769,3.876716], 12);
+var mymap = L.map('mapid').setView([43.610769,3.876716], 13);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 maxZoom: 18,
